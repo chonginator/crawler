c50bd6561b0d000332dc7b5f54f38c7ae6db417c
chonginator <benchong1999@yahoo.com.au>
2024-09-03 19:40:11 +1000

    add main function

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	argsWithoutProg := os.Args[1:]
+
+	if len(argsWithoutProg) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+	if len(argsWithoutProg) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	rawBaseURL := argsWithoutProg[0]
+
+	fmt.Println("starting crawl of:", rawBaseURL)
 }
